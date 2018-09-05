61e6b2d096e07b76dd1cf578b503e4512c3c0d7a
Carlos López <carloslopez@macbookpro.local>
2018-09-05 10:44:31 -0500

    iniciar commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+	appname: 'music',
+	apiKey: '3b781a1cb7cb45947fb890d5f9b25712',
+	secret: 'b4aefceab80e10dc142726ae851c7dc5',
+	registeredTo: 'carloslopez1201' 
+}
+
+
+// Application name	music
+// API key	3b781a1cb7cb45947fb890d5f9b25712
+// Shared secret	b4aefceab80e10dc142726ae851c7dc5
+// Registered to	carloslopez1201 
